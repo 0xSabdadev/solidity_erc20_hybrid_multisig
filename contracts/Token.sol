15be7f89563ea81d4f032839137aefee3dd26f07
0xSabdadev <jasonalhilal@gmail.com>
2023-10-27 02:03:23 +0700

    p3_finished

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20{
-    constructor() ERC20 ('JASON','JSN'){
+    constructor() ERC20 ('DIYUT','DYT'){
         _mint(msg.sender, 10000000000000000000000000);
     }
     function _approve(address owner, uint amount) public{
